661d2d6d9be65b1991a2501a8c264f8ad06e8165
Yash Bhojgarhia <yash.bhojgarhia@jeevansathi.com>
2021-06-21 17:14:30 +0530

    added buttons

--- Day One Clone/CreateJournalViewController.swift
+++ Day One Clone/CreateJournalViewController.swift
@@ -11,8 +11,6 @@ class CreateJournalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
--- Day One Clone/JournalTableViewController.swift
+++ Day One Clone/JournalTableViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class JournalTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var whiteCameraButton: UIButton!
+    @IBOutlet weak var whitePlusButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        whiteCameraButton.imageView?.contentMode = .scaleAspectFit
+        whitePlusButton.imageView?.contentMode = .scaleAspectFit
     }
 
     // MARK: - Table view data source
@@ -30,60 +30,17 @@ class JournalTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToNew", sender: "camera")
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goToNew", sender: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
 }
