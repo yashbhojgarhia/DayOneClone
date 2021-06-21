c03593d431476524c1beb576f200f206ca89582f
Yash Bhojgarhia <yash.bhojgarhia@jeevansathi.com>
2021-06-21 16:40:24 +0530

    Storyboard setup

---
+++ Day One Clone/CreateJournalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateJournalViewController.swift
+//  Day One Clone
+//
+//  Created by Yash Bhojgarhia on 21/06/21.
+//
+
+import UIKit
+
+class CreateJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
